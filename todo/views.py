765f57ed83101eb7c521470c6840ae61a2d7b9de
CORENTIN PASQUIER <43130282+PasquierCorentin@users.noreply.github.com>
2020-12-02 17:47:39 +0100

    Add files via upload

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 
 def index(request):
     todos = Todo.objects.all()
 
-    context = {'todos':todos}
+    form = TodoForm()
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+    context = {'todos':todos, 'form':form}
     return render(request, 'todo/list.html', context)
+
+
+def updateTodo(request, pk):
+    todo =Todo.objects.get(id=pk)
+
+    form = TodoForm(instance=todo)
+
+    if request.method == 'POST': 
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+
+    return render(request,'todo/update_todo.html', context)
+
+def deleteTodo(request, pk):
+    item = Todo.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item':item}
+    return render(request, 'todo/delete.html', context)
